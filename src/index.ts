be4b0f62ccd62d6449a046147710f45bf9166359
Brandon Lee Dring <Brandon.Dring@cdk.com>
2017-10-27 22:34:28 -0700

    Attempt at unrar-ing

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
 class extractMe {
-   private program;
-   private unzip;
-   private _;
-   private fs;
-   private sevenZ;
-   private _7z;
-   private decomp;
+   private program = require("commander");
+   private unzip = require('unzip');
+   private _ = require("lodash");
+   private fs = require('fs');
+   private sevenZ = require("node-7z");
+   private _7z = new this.sevenZ();
+   private decomp = require("decompress");
+   private rar = require("node-unrar");
 
    constructor() {
-      this.initParsers();
       this.catchCommands();
-   }
-
-
-   private initParsers(): void {
-      this.program = require("commander")
-      this.unzip = require('unzip')
-      this._ = require("lodash")
-      this.fs = require('fs')
-      this.sevenZ = require("node-7z")
-      this._7z = new this.sevenZ();
-      this.decomp = require("decompress");
-
-
    }
    private catchCommands(): void {
       this.program
@@ -45,6 +32,19 @@ class extractMe {
          this.catch7Zip(file)
       else if (this._.includes(file, ".tar"))
          this.catchTar(file)
+      else if (this._.includes(file, ".rar"))
+         this.catchRAR(file)
+      else
+         console.log("File type not supported (yet)!")
+   }
+   private catchRAR(file: string): void {
+      let rar = new this.rar(process.cwd() + "/" + file)
+      console.log(rar)
+      rar.extract("./", null, err => {
+         console.log("weee")
+         throw err
+      })
+
    }
 
    private catchTar(file: string): void {
@@ -54,8 +54,6 @@ class extractMe {
    private catch7Zip(file: string): void {
       this._7z.extract(process.cwd() + "/" + file, "./");
    }
-
-
 
    private catchZip(file: string): void {
       this.fs.createReadStream(process.cwd() + "/" + file)
